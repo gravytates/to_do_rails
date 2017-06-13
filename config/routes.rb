b48c0488ee06d85fe4bffdda2cf0de4ae6e152eb
Grady Shelton and Susha Dore <grady@>
2017-06-12 20:03:07 -0700

    add bootstrap to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  resources :todos
   resources :lists do
     resources :tasks
   end
 
   resources :tasks do
-    
   end
+
+  resources :todos
+  root 'todos#index'
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608015206) do
+ActiveRecord::Schema.define(version: 20170613024848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20170608015206) do
   create_table "tasks", force: :cascade do |t|
     t.string "description"
     t.integer "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
